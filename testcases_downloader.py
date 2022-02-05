c2dfe4805d87294d8e30ca86b137edd3e4693e91
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2022-02-05 16:27:46 +0530

    updating files & debugging

--- testcases_downloader.py
+++ testcases_downloader.py
@@ -5,7 +5,8 @@ from selenium.webdriver.common.by import By
 # from demo import url
 import os
 
-url = input("Enter The URL : ")
+url = input("Enter The Problem URL : ")
+url = "https://codeforces.com/problemset/problem/1633/E"
 
 pwd = os.getcwd()
 PATH = Service(pwd + "/chromedriver")
@@ -17,19 +18,26 @@ chromeOptions.add_argument("--disable-notifications") # chromeOptions.add_experi
 
 # driver setup:
 driver = webdriver.Chrome(service = PATH, options = chromeOptions) # driver.maximize_window() driver.minimize_window()
-# driver.get(url) # launches the broswer and open url
+driver.get(url) # launches the broswer and open url
 
 def download_testcases():
 
-   pb_arr = []
+   # initialising URL as elements into an array list
+   pb_url_arr = []
    for i in url:
-      pb_arr.append(i)
-   pb_char = pb_arr[len(pb_arr) - 1]
-   print(pb_char)
+      pb_url_arr.append(i)
+   pb_char = pb_url_arr[len(pb_url_arr) - 1]
+   print("Problem No. : " + pb_char)
 
+   
+   # scrapped sample testcases with garbage texts
    sample_testcases = driver.find_element(By.XPATH, "//div[@class='sample-tests']").text
    sample_testcases = sample_testcases + "\nabhijayrajvansh"
+   print()
+   print(sample_testcases)
+   
 
+   # initialising scrapped data testcases into an array list
    arr = []
    temp = ""
    for i in sample_testcases:
@@ -38,10 +46,15 @@ def download_testcases():
          temp = ""
       else:
          temp += i
-   # print(arr)
+   print('\n')
+   print(arr) 
 
-   inputfile = open(pb_char + "/input.txt", "w")
-   outputfile = open(pb_char + "/output.txt", "w")
+
+
+
+
+   inputfile = open("input.txt", "w")
+   outputfile = open("output.txt", "w")
 
    n = len(arr) 
    i = 3
@@ -56,5 +69,6 @@ def download_testcases():
       outputfile.write(arr[j] + '\n')
       j += 1
    
+download_testcases()
 
 
