68880505f8a9c9725dab39da10375ae8636d78f8
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2022-02-19 01:04:43 +0530

    updating files & debugging

--- config.fish
+++ config.fish
@@ -1,28 +1,57 @@
-
-    #Codeforces Contest Bot
+#Codeforces Contest Bot
     function cf_contest_bot
         # Directory location of Codeforces Bot
         /Users/abhijayrajvansh/Desktop/development/projects/Codeforces-Contest-Bot && python main.py $argv
         # After running bot and downloading TC
-        codeforces 
+        codeforces
         cd $argv && cd A
     end
     function cf-upsolver
         #directory of upsolver bot
         /Users/abhijayrajvansh/Desktop/development/projects/Codeforces-Contest-Bot && python cf-upsolver.py $argv
         #after running bot and downloading testcases
-        /Users/abhijayrajvansh/desktop/onlineJudge/codeforces/upsolve
+        /Users/abhijayrajvansh/desktop/codeforces/upsolve
     end
+
     
     # CommandLine Debug during contests:- Compile, Debug && Timed: C++
     function deb # ABHIJAY_DEBUG FILE_NAME.cpp
-        echo "[ABHIJAY_DEBUG MODE] Compiling" $argv" with G++17..."
+        echo "[ABHIJAY_DEBUG MODE] Compiling" $argv.cpp" with G++17..."
+        echo
         echo "Input: "
-        g++ -std=c++17 -DABHIJAY_DEBUG $argv -o a.out && ./a.out
+        cat sample_input_1.txt
+        echo "``````````````````````````````````````````````````````"
+        g++ -std=c++17 -DABHIJAY_DEBUG $argv.cpp -o a.out
+        sleep 1
+        ./a.out
+        
+    end
+
+
+    function run # ABHIJAY_DEBUG FILE_NAME.cpp
+        echo "Compiling" $argv.cpp" with G++17..."
+        echo
+        g++ -std=c++17 $argv.cpp -o a.out
+        sleep 1
+        if test -f sample_input_1.txt;
+            ./a.out <sample_input_1.txt> my_output_1.txt
+            if cmp -s sample_output_1.txt my_output_1.txt;
+                echo Running Testcase 1:(set_color --bold green) 'Passed!' 
+                echo " Expected                                My Output"
+                echo "```````````                             ````````````"
+                diff -y -W 70 sample_output_1.txt my_output_1.txt
+            else
+                echo Running Testcase 1:(set_color --bold red) 'Failed' 
+                echo " Expected                                My Output"
+                echo "```````````                             ````````````"
+                diff -y -W 70 sample_output_1.txt my_output_1.txt
+            end
+        end
     end
 
     function runsamples # runnig and testing sample testcases
         echo "[ABHIJAY_DEBUG MODE] Compiling main.cpp with G++17." \n
+        sleep 1
         #navigating to problem dir:
         cd ..
         cd $argv
@@ -83,22 +112,17 @@
             end
         end
     end
-    function db # runnig  and testing sample testcases
+    function runtestcases # runnig  and testing sample testcases
         echo "[ABHIJAY_DEBUG MODE] Compiling main.cpp with G++17." \n
+        sleep 1
         # Compiling file
-        g++ -std=c++17 $argv.cpp -o $argv.out
+        g++ -std=c++17 main.cpp -o main.out
         #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         # Running testcases:
         if test -f sample_input_1.txt;
-            ./$argv.out <sample_input_1.txt> my_output_1.txt
+            ./main.out <sample_input_1.txt> my_output_1.txt
             if cmp -s sample_output_1.txt my_output_1.txt;
                 echo Running Testcase 1:(set_color --bold green) 'Passed!' (set_color normal)
-                set_color green              
-                echo "Expected                              My Output"
-                echo "``````````                           ````````````"
-                diff -y -W 70 sample_output_1.txt my_output_1.txt
-                echo ""
-                set_color normal
             else
                 echo Running Testcase 1:(set_color --bold red) 'Failed' (set_color normal)                
                 echo "Expected                              My Output"
@@ -108,15 +132,9 @@
             end
         end
         if test -f sample_input_2.txt;
-            ./$argv.out <sample_input_2.txt> my_output_2.txt
+            ./main.out <sample_input_2.txt> my_output_2.txt
             if cmp -s sample_output_2.txt my_output_2.txt;
                 echo Running Testcase 2:(set_color --bold green) 'Passed!' (set_color normal)
-                set_color green              
-                echo "Expected                              My Output"
-                echo "``````````                           ````````````"
-                diff -y -W 70 sample_output_1.txt my_output_1.txt
-                echo ""
-                set_color normal
             else
                 echo Running Testcase 2:(set_color --bold red) 'Failed' (set_color normal)                
                 echo "Expected                               My Output"
@@ -126,15 +144,9 @@
             end
         end
         if test -f sample_input_3.txt;
-            ./$argv.out <sample_input_3.txt> my_output_3.txt
+            ./main.out <sample_input_3.txt> my_output_3.txt
             if cmp -s sample_output_3.txt my_output_3.txt;
                 echo Running Testcase 3:(set_color --bold green) 'Passed!' (set_color normal)
-                set_color green              
-                echo "Expected                              My Output"
-                echo "``````````                           ````````````"
-                diff -y -W 70 sample_output_1.txt my_output_1.txt
-                echo ""
-                set_color normal
             else
                 echo Running Testcase 3:(set_color --bold red) 'Failed' (set_color normal)                
                 echo "Expected                               My Output"
@@ -174,5 +186,3 @@
             diff -y -W 70 sample_output_1.txt my_output_1.txt
         end
     end
-    
-    # alias deb-main='echo "[ABHIJAY_DEBUG MODE] Compiling main.cpp with G++17..." && g++ -DABHIJAY_DEBUG main.cpp -o main && ./main'
